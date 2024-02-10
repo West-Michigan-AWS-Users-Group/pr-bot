3ade1a959bb49d48d20867007ce5a8c391ed0f1a
tnielsen2 <trentnielsen84@gmail.com>
2024-02-09 23:13:39 -0500

    Modify Lambda to parse event body into dict

--- cdk/dev-lambda/dev_review_pr.py
+++ cdk/dev-lambda/dev_review_pr.py
@@ -2,12 +2,13 @@ import json
 import logging
 
 logger = logging.getLogger()
-logger.setLevel('INFO')
-
+logger.setLevel(logging.INFO)
 
 def handler(event, context):
     try:
-        logger.info('Event data: ' + json.dumps(event))
+        event_body = json.loads(event['body'])
+        logger.info('event: %s', json.dumps(event))
+        logger.info('event["body"]: %s', json.dumps(event_body))
     except Exception as e:
         message = f'Lambda function execution failed: {e}'
         logger.error(message)
