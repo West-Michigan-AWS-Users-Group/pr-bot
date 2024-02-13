767d745ec890cb580c85bc93161be56c64d2ebd7
tnielsen2 <trentnielsen84@gmail.com>
2024-02-12 20:43:11 -0500

    Add skeleton for github auth

--- cdk/dev-lambda/dev_review_pr.py
+++ cdk/dev-lambda/dev_review_pr.py
@@ -1,4 +1,5 @@
 import json
+from github import Auth, Github, PullRequest
 import logging
 
 logger = logging.getLogger()
@@ -7,11 +8,19 @@ logger.setLevel(logging.INFO)
 
 def handler(event, context):
     try:
-        event_body = json.loads(event["body"])
+        github_webhook = json.loads(event["body"])
         logger.info("event: %s", json.dumps(event))
-        logger.info('event["body"]: %s', json.dumps(event_body))
+        logger.info('event["body"]: %s', json.dumps(github_webhook))
     except Exception as e:
         message = f"Lambda function execution failed: {e}"
         logger.error(message)
         return {"statusCode": 500, "body": message}
+
+    auth = Auth.Token("placeholder")
+    g = Github(auth=auth)
+    pr_number = 2
+    repo = g.get_repo("West-Michigan-AWS-Users-Group/pr-bot")
+    pr = repo.get_pull(pr_number)
+    pr.create_issue_comment("Test comment from pr-bot development environment")
+    
     return {"statusCode": 200, "body": json.dumps(event)}
