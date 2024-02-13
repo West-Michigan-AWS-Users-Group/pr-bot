b2589cf0d392a1826a963dace3355a711d7eee88
tnielsen2 <trentnielsen84@gmail.com>
2024-02-13 12:44:45 -0500

    Fix quoting

--- cdk/create-layer.sh
+++ cdk/create-layer.sh
@@ -5,7 +5,7 @@ echo "Virtual environment created"
 ENV=$1
 
 # Install packages into the virtual environment without specifying a custom target directory
-pip install --platform manylinux2010_x86_64 --only-binary=:all: langchain=0.0.5 PyGithub="1.54.1" -t ./python -v
+pip install --platform manylinux2010_x86_64 --only-binary=:all: langchain="0.0.5" PyGithub="1.54.1" -t ./python -v
 echo "Packages installed to ./$ENV-lambda/layer.zip"
 
 # Create the zip file preserving the entire site-packages directory structure
