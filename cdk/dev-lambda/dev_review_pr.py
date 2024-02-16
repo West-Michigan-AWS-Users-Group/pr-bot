c2a538334124273fe113f3d8af0cd5c3de0ac17a
tnielsen2 <trentnielsen84@gmail.com>
2024-02-15 20:34:35 -0500

    Add logic to only use bedrock if a PR is `opened`, add prompt template source and target ref values

--- cdk/dev-lambda/dev_review_pr.py
+++ cdk/dev-lambda/dev_review_pr.py
@@ -58,11 +58,12 @@ def format_diff(diff: str) -> str:
     return diff
 
 
-def prompt_bedrock(diff_code: str) -> str:
+def prompt_bedrock(diff_code: str, source_ref: str, target_ref: str) -> str:
     """
     Generate a prompt for the Bedrock API
     diff_code: str: Multi-line GitHub diff output from a pull request.
-
+    source_ref: str: The source branch of the pull request.
+    target_ref: str: The target branch of the pull request.
     """
 
     # Bedrock configuration values
@@ -81,7 +82,7 @@ def prompt_bedrock(diff_code: str) -> str:
     )
 
     pr_review_prompt = PromptTemplate(
-        input_variables=["diff"],
+        input_variables=["diff", "source_branch", "target_branch"],
         template="""
 
 Human: You are being provided a diff of code changes in a PR. The diff needs to be reviewed for any potential issues.
@@ -103,7 +104,7 @@ If there are less than 10 bullet points, that is okay. If there are more than 10
 most important points. Post this message in markdown formatting. At the start of the response, please include source
 branch and target branch of the PR in the following format:
  
-"## `source_branch` --> `target_branch` "
+"## `{source_branch}` --> `{target_branch}` "
 
 Be sure to include the arrow between the source and target branches and make this a Heading2 in markdown.
 
@@ -116,7 +117,9 @@ without quotes and ensure it is italicised.
 Assistant:""",
     )
 
-    prompt = pr_review_prompt.format(diff=diff_code)
+    prompt = pr_review_prompt.format(
+        diff=diff_code, source_branch=source_ref, target_branch=target_ref
+    )
     logger.info("prompt generated successfully")
     logger.debug("prompt: %s", prompt)
     response = textgen_llm(prompt)
@@ -172,56 +175,63 @@ def handler(event, context):
     if pull_request_event := github_webhook.get("pull_request"):
         logger.info("pull_request_event: %s", pull_request_event)
         # assign required values to python variables
-        try:
-            pr_number = github_webhook["number"]
-            logger.info("pr_number: %s", pr_number)
-            repo_name = github_webhook["repository"]["full_name"]
-            logger.info("repo: %s", repo_name)
-            pr_diff_url = github_webhook["pull_request"]["url"]
-            logger.info("pr_diff_url: %s", pr_diff_url)
-        except Exception as e:
-            message = f"Error assigning required values to python variables: {e}"
-            logger.error(message)
-            return {"statusCode": 500, "body": message}
+        if github_webhook["action"] == "opened":
+            logger.info("action: %s", github_webhook["action"])
+            try:
+                pr_number = github_webhook["number"]
+                logger.info("pr_number: %s", pr_number)
+                repo_name = github_webhook["repository"]["full_name"]
+                logger.info("repo: %s", repo_name)
+                pr_diff_url = github_webhook["pull_request"]["url"] + ".diff"
+                source_ref = github_webhook["pull_request"]["head"]["ref"]
+                target_ref = github_webhook["pull_request"]["base"]["ref"]
+                logger.info("pr_diff_url: %s", pr_diff_url)
+            except Exception as e:
+                message = f"Error assigning required values to python variables: {e}"
+                logger.error(message)
+                return {"statusCode": 500, "body": message}
 
-        # fetch diff url contents
-        try:
-            diff = get_diff_from_pr(pr_diff_url, github_token)
-            # replace the exact string Human and Assistant with empty string to prevent LLM confusion
-            logger.info("diff fetched successfully")
-            diff = format_diff(diff)
-            logger.info("diff formatted output: \n%s", diff)
-        except Exception as e:
-            message = f"Error fetching diff url: {e}"
-            logger.error(message)
-            return {"statusCode": 500, "body": message}
+            # fetch diff url contents
+            try:
+                diff = get_diff_from_pr(pr_diff_url, github_token)
+                # replace the exact string Human and Assistant with empty string to prevent LLM confusion
+                logger.info("diff fetched successfully")
+                diff = format_diff(diff)
+                logger.info("diff formatted output: \n%s", diff)
+            except Exception as e:
+                message = f"Error fetching diff url: {e}"
+                logger.error(message)
+                return {"statusCode": 500, "body": message}
 
-        try:
-            logger.info("Prompting bedrock with diff.")
-            bedrock_response = prompt_bedrock(diff)
-            logger.info("bedrock response: %s", bedrock_response)
-        except Exception as e:
-            message = f"Error generating bedrock response: {e}"
-            logger.error(message)
-            return {"statusCode": 500, "body": message}
+            try:
+                logger.info("Prompting bedrock with diff.")
+                bedrock_response = prompt_bedrock(diff, source_ref, target_ref)
+                logger.info("bedrock response: %s", bedrock_response)
+            except Exception as e:
+                message = f"Error generating bedrock response: {e}"
+                logger.error(message)
+                return {"statusCode": 500, "body": message}
 
-        # auth with github and post comment to pr
-        try:
-            g_session = authenticate_github(github_token)
-            repo = g_session.get_repo(repo_name)
-            pr = repo.get_pull(pr_number)
-            logger.info("pr: %s", pr)
-            post_comment_to_pr(
-                pr,
-                bedrock_response,
-            )
-        except Exception as e:
-            message = f"Error posting comment to pr: {e}"
-            logger.error(message)
-            return {"statusCode": 500, "body": message}
+            # auth with github and post comment to pr
+            try:
+                g_session = authenticate_github(github_token)
+                repo = g_session.get_repo(repo_name)
+                pr = repo.get_pull(pr_number)
+                logger.info("pr: %s", pr)
+                post_comment_to_pr(
+                    pr,
+                    bedrock_response,
+                )
+            except Exception as e:
+                message = f"Error posting comment to pr: {e}"
+                logger.error(message)
+                return {"statusCode": 500, "body": message}
 
-        logger.info("Comment posted to PR successfully.")
-        return {"statusCode": 200, "body": "Comment posted to PR successfully."}
+            logger.info("Comment posted to PR successfully.")
+            return {"statusCode": 200, "body": "Comment posted to PR successfully."}
+        else:
+            logger.info("pull_request action != opened, so no further action is taken.")
+            return {"statusCode": 200, "body": json.dumps(event)}
     else:
-        logger.info("No pull_request event found in the webhook")
+        logger.info("No pull_request action found in the webhook")
         return {"statusCode": 200, "body": json.dumps(event)}
